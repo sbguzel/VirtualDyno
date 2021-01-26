27de99135ea4d1cb67aa095453864d7a963da960
Burak Güzel <44478688+BurakYtu@users.noreply.github.com>
2021-01-26 19:29:38 +0300

    timestep added
    Time difference between two data in ms was added.

--- ReadOBD/ReadOBD/Program.cs
+++ ReadOBD/ReadOBD/Program.cs
@@ -8,8 +8,10 @@ namespace ReadOBD
         {
             String[] lines;
             int[] speed_kph = new int[5000];
+            int[] speed_time = new int[5000];
             string hex;
-
+            string time_string;
+            Int32 time_ms = 0, time_ms_diff = 0, time_offset = 0, time_ms_old = 0;
             lines = System.IO.File.ReadAllLines(@"D:\saidburakguzel\Projects\Practice\C#\OBD2_read\ReadOBD\OBD_Log.txt");
             //System.Console.WriteLine("Contents of WriteLines2.txt = ");
             //foreach (string line in lines)
@@ -43,7 +45,27 @@ namespace ReadOBD
                                     hex = lines[i + 1][22].ToString() + lines[i + 1][23].ToString();
                                     //Console.WriteLine("Hex:" + hex);
                                     speed_kph[j] = int.Parse(hex, System.Globalization.NumberStyles.HexNumber);
-                                    Console.WriteLine(speed_kph[j]);
+                                    
+                                    time_string = lines[i + 1][0].ToString() + lines[i + 1][1].ToString() + lines[i + 1][3].ToString() + lines[i + 1][4].ToString() + lines[i + 1][5].ToString();
+                                    time_ms = Int32.Parse(time_string);
+
+                                    if(j == 0)
+                                    {
+                                        speed_time[j] = 0;
+                                    }
+                                    else
+                                    {
+                                        time_ms_diff = time_ms - time_ms_old;
+                                        if(time_ms_diff < 0)
+                                        {
+                                            time_ms_diff += 60000;
+                                        }
+                                        speed_time[j] = time_ms_diff;
+                                    }
+
+                                    Console.WriteLine(speed_time[j] + " " +speed_kph[j]);
+
+                                    time_ms_old = time_ms;
                                     j += 1;
                                 }
                                 
