3fc3ebfcd4641ed9ecfdaa020471bc0658d42113
Burak Güzel <44478688+BurakYtu@users.noreply.github.com>
2021-01-30 11:09:06 +0300

    Fuel rail pressure and EGR value added
    function will be added

--- ReadOBD/ReadOBD/Program.cs
+++ ReadOBD/ReadOBD/Program.cs
@@ -9,22 +9,27 @@ namespace ReadOBD
             String[] lines;
             int[] speed_kph = new int[5000];
             int[] rpm = new int[5000];
+            int[] fuel_rail_gauge_pressure = new int[5000];
+            int[] egr_percent = new int[5000];
             int[] speed_time = new int[5000];
             int[] rpm_time = new int[5000];
+            int[] fuel_rail_gauge_pressure_time = new int[5000];
+            int[] egr_time = new int[5000];
 
-            string hex, hexA, hexB;
+            string hexA, hexB;
             int A = 0, B = 0;
             string time_string;
             Int32 time_ms = 0, time_ms_diff = 0, time_ms_old = 0;
             lines = System.IO.File.ReadAllLines(@"D:\saidburakguzel\Projects\Practice\C#\OBD2_read\ReadOBD\OBD_Log.txt");
+            int j = 0;
 
+            //Console.WriteLine(lines.Length.ToString());
 
-            Console.WriteLine(lines.Length.ToString());
 
             string speed_code = "010D";
-            int j = 0;
+            j = 0;
             
-            for (int i = 0; i < lines.Length; i++)
+            for (int i = 249; i < lines.Length; i++)
             {
                 if (!string.IsNullOrEmpty(lines[i]))
                 {
@@ -39,16 +44,17 @@ namespace ReadOBD
                                 length = lines[i+1].Length;
                                 if (length >= 23)
                                 {
-                                    hex = lines[i + 1][22].ToString() + lines[i + 1][23].ToString();
-                                    //Console.WriteLine("Hex:" + hex);
-                                    speed_kph[j] = int.Parse(hex, System.Globalization.NumberStyles.HexNumber);
+                                    hexA = lines[i + 1][22].ToString() + lines[i + 1][23].ToString();
+                                    A = int.Parse(hexA, System.Globalization.NumberStyles.HexNumber);
+                                    speed_kph[j] = int.Parse(hexA, System.Globalization.NumberStyles.HexNumber);
                                     
                                     time_string = lines[i + 1][0].ToString() + lines[i + 1][1].ToString() + lines[i + 1][3].ToString() + lines[i + 1][4].ToString() + lines[i + 1][5].ToString();
                                     time_ms = Int32.Parse(time_string);
 
+
                                     if(j == 0)
                                     {
-                                        speed_time[j] = 0;
+                                        speed_time[j] = time_ms;
                                     }
                                     else
                                     {
@@ -82,7 +88,7 @@ namespace ReadOBD
             j = 0;
             time_ms_old = 0;
 
-            for (int i = 0; i < lines.Length; i++)
+            for (int i = 249; i < lines.Length; i++)
             {
                 if (!string.IsNullOrEmpty(lines[i]))
                 {
@@ -108,7 +114,7 @@ namespace ReadOBD
 
                                     if (j == 0)
                                     {
-                                        rpm_time[j] = 0;
+                                        rpm_time[j] = time_ms;
                                     }
                                     else
                                     {
@@ -136,6 +142,132 @@ namespace ReadOBD
                 }
 
             }
+
+
+
+            string fuel_rail_gauge_pressure_code = "0123";
+            j = 0;
+            time_ms_old = 0;
+
+            for (int i = 249; i < lines.Length; i++)
+            {
+                if (!string.IsNullOrEmpty(lines[i]))
+                {
+                    int length = lines[i].Length;
+                    if (length > 12)
+                    {
+                        string temp = lines[i][9].ToString() + lines[i][10].ToString() + lines[i][11].ToString() + lines[i][12].ToString();
+                        if (String.Compare(fuel_rail_gauge_pressure_code, temp) == 0)
+                        {
+                            if (!string.IsNullOrEmpty(lines[i + 1]))
+                            {
+                                length = lines[i + 1].Length;
+                                if (length >= 26)
+                                {
+                                    hexA = lines[i + 1][22].ToString() + lines[i + 1][23].ToString();
+                                    hexB = lines[i + 1][25].ToString() + lines[i + 1][26].ToString();
+                                    A = int.Parse(hexA, System.Globalization.NumberStyles.HexNumber);
+                                    B = int.Parse(hexB, System.Globalization.NumberStyles.HexNumber);
+                                    fuel_rail_gauge_pressure[j] = (256 * A + B) * 10;
+
+                                    time_string = lines[i + 1][0].ToString() + lines[i + 1][1].ToString() + lines[i + 1][3].ToString() + lines[i + 1][4].ToString() + lines[i + 1][5].ToString();
+                                    time_ms = Int32.Parse(time_string);
+
+                                    if (j == 0)
+                                    {
+                                        fuel_rail_gauge_pressure_time[j] = time_ms;
+                                    }
+                                    else
+                                    {
+                                        time_ms_diff = time_ms - time_ms_old;
+                                        if (time_ms_diff < 0)
+                                        {
+                                            time_ms_diff += 60000;
+                                        }
+                                        fuel_rail_gauge_pressure_time[j] = fuel_rail_gauge_pressure_time[j - 1] + time_ms_diff;
+
+                                    }
+
+                                    Console.WriteLine(fuel_rail_gauge_pressure_time[j] + " " + fuel_rail_gauge_pressure[j]);
+
+                                    time_ms_old = time_ms;
+                                    j += 1;
+                                }
+
+                            }
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+            
+            string EGR_code = "012C";
+            j = 0;
+            time_ms_old = 0;
+
+            for (int i = 249; i < lines.Length; i++)
+            {
+                if (!string.IsNullOrEmpty(lines[i]))
+                {
+                    int length = lines[i].Length;
+                    if (length > 12)
+                    {
+                        string temp = lines[i][9].ToString() + lines[i][10].ToString() + lines[i][11].ToString() + lines[i][12].ToString();
+                        if (String.Compare(EGR_code, temp) == 0)
+                        {
+                            if (!string.IsNullOrEmpty(lines[i + 1]))
+                            {
+                                length = lines[i + 1].Length;
+                                if (length >= 23)
+                                {
+                                    hexA = lines[i + 1][22].ToString() + lines[i + 1][23].ToString();
+                                    A = int.Parse(hexA, System.Globalization.NumberStyles.HexNumber);
+                                    egr_percent[j] = A * 100 / 255;
+
+                                    time_string = lines[i + 1][0].ToString() + lines[i + 1][1].ToString() + lines[i + 1][3].ToString() + lines[i + 1][4].ToString() + lines[i + 1][5].ToString();
+                                    time_ms = Int32.Parse(time_string);
+
+                                    if (j == 0)
+                                    {
+                                        egr_time[j] = time_ms;
+                                    }
+                                    else
+                                    {
+                                        time_ms_diff = time_ms - time_ms_old;
+                                        if (time_ms_diff < 0)
+                                        {
+                                            time_ms_diff += 60000;
+                                        }
+                                        egr_time[j] = egr_time[j - 1] + time_ms_diff;
+
+                                    }
+
+                                    Console.WriteLine(egr_time[j] + " " + egr_percent[j]);
+
+                                    time_ms_old = time_ms;
+                                    j += 1;
+                                }
+
+                            }
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+
+
+        int Read_obd(int start_row, int )
+        {
 
         }
 
